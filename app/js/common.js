46ae6756817c29fd73d41a399b1021a47a36b9fa
FederalDrop <federaldrop88@gmail.com>
2022-12-16 23:57:23 +0300

    update

--- app/js/common.js
+++ app/js/common.js
@@ -9,7 +9,6 @@ $(function () {
 	});
 	$('.partners__slider').slick({
 		slidesToShow: 3,
-		slidesToScroll: 3,
 		dots: true,
 	});
 	$('.med__slider').slick({
@@ -19,8 +18,12 @@ $(function () {
 	$('.effect__slider').slick({
 		dots: true,
 	});
+	$('.reviews__slider').slick({
+		dots: true,
+	});
 	$('.about__slider').slick({
 		dots: true,
+		vertical: true,
 	});
 	$('.popular__slider').slick({
 		slidesToShow: 3,
@@ -30,10 +33,65 @@ $(function () {
 		slidesToShow: 3,
 		dots: true,
 	});
+	$('a[href*="#"]').on('click.smoothscroll', function (e) {
+		var hash = this.hash, _hash = hash.replace(/#/, ''), theHref = $(this).attr('href').replace(/#.*/, '');
+		if (theHref && location.href.replace(/#.*/, '') != theHref) return;
+		var $target = _hash === '' ? $('body') : $(hash + ', a[name="' + _hash + '"]').first();
+		if (!$target.length) return;
+		e.preventDefault();
+		$('html, body').stop().animate({ scrollTop: $target.offset().top - 0 }, 800, 'swing', function () {
+			window.location.hash = hash;
+		});
+	});
 
 });
 
 window.addEventListener('load', function () {
+
+	const onScrollHeader = () => { // объявляем основную функцию onScrollHeader
+
+		const header = document.querySelector('.header') // находим header и записываем в константу
+
+		let prevScroll = window.pageYOffset // узнаем на сколько была прокручена страница ранее
+		console.log(prevScroll);
+		let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
+
+		window.addEventListener('scroll', () => { // при прокрутке страницы
+			currentScroll = window.pageYOffset // узнаем на сколько прокрутили страницу
+			const headerHidden = () => header.classList.contains('header_bg') // узнаем скрыт header или нет
+			if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
+				header.classList.add('header_bg') // то скрываем header
+			}
+			if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
+				header.classList.remove('header_bg') // то отображаем header
+			}
+			prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
+		})
+	}
+	onScrollHeader() // вызываем основную функцию onScrollHeader
+
+	let reviewsText = document.querySelectorAll('.reviews-item__text');
+	reviewsText.forEach(element => {
+		element.nextElementSibling.addEventListener('click', event => {
+			element.classList.toggle('down');
+			let answer = element;
+			if (element.classList.contains('down')) {
+				answer.style.maxHeight = answer.scrollHeight + 'px';
+				answer.style.opacity = '1';
+			}
+			else {
+				answer.style.maxHeight = '92px';
+				answer.style.opacity = '1';
+			}
+		})
+	})
+
+	let dots = document.querySelectorAll('.dots-item');
+	dots.forEach(element => {
+		element.addEventListener('click', event => {
+			element.classList.toggle('down');
+		})
+	})
 
 	let vipad = document.querySelectorAll('.faq-item__plug');
 	let down = document.querySelector('.faq-answer__wrappper');
@@ -69,5 +127,88 @@ window.addEventListener('load', function () {
 		})
 	})
 
+	function popupAir() {
+		const footerElement = document.querySelector('footer');
+		if (!footerElement) {
+			alert('dont find teg footer')
+		}
+		else {
+			let airElements = document.querySelectorAll('.popup-air');
+			if (airElements.length > 0) {
+				let airBtnOpen = document.querySelectorAll('.air-open-btn');
+				createAirPopups()
+
+				for (let i = 0; i < airBtnOpen.length; i++) {
+					airBtnOpen[i].onclick = openAirPopup
+				}
+			}
+			else {
+				return
+			}
+
+			function createAirPopups() {
+				let airConteiner = document.createElement("div");
+				airConteiner.classList.add('air-conteiner');
+
+				for (let i = 0; i < airElements.length; i++) {
+					let airCloseIcon = document.createElement("div");
+					airCloseIcon.classList.add('air-close');
+					airElements[i].appendChild(airCloseIcon)
+					airConteiner.appendChild(airElements[i])
+
+				}
+				footerElement.after(airConteiner)
+			}
+
+			function openAirPopup() {
+				let currentAirPopupBtn = this.getAttribute('data-popup-current');
+				let allPopups = document.querySelectorAll('.popup-air');
+				let currentAirPopup = document.querySelector(`.popup-air[data-air="${currentAirPopupBtn}"]`);
+				let closeAirIcon = currentAirPopup.querySelector('.air-close');
+				closeAllAirPopups(allPopups);
+				openAirConteiner();
+				currentAirPopup.classList.add('air-popup_active');
+
+				closeAirIcon.addEventListener('click', function () {
+					currentAirPopup.classList.remove('air-popup_active');
+					closeAirConteiner()
+				})
+			}
+
+			function openAirConteiner() {
+				let airConteier = document.querySelector('.air-conteiner');
+				airConteier.classList.add('air-conteiner_active');
+			}
+
+			function closeAllAirPopups(allPopups) {
+				for (let i = 0; i < allPopups.length; i++) {
+					allPopups[i].classList.remove('air-popup_active');
+				}
+			}
+			function closeAirConteiner() {
+				let airConteier = document.querySelector('.air-conteiner');
+				airConteier.classList.remove('air-conteiner_active');
+			}
+		}
+
+	}
+	popupAir()
+
+	function videoLoad() {
+		let videoBtn = document.querySelectorAll('.video-btn');
+		for (let i = 0; i < videoBtn.length; i++) {
+			videoBtn[i].addEventListener('click', function () {
+				let videoUrl = this.getAttribute('data-video-src');
+				let closeVideoBtn = document.querySelector('.video-popup').closest('.popup-air').querySelector('.air-close');
+				document.querySelector('.video-popup iframe').setAttribute('src', videoUrl)
+				closeVideoBtn.addEventListener('click', function () {
+					setTimeout(function () {
+						document.querySelector('.video-popup iframe').setAttribute('src', '')
+					}, 800)
+				});
+			})
+		}
+	}
+	videoLoad()
 
 }, false);
